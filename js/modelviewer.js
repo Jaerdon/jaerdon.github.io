90a04b679157c781e97709eb62df9a6280bede1b
Jaerdon <jordandavidwhiting@gmail.com>
2019-06-03 12:49:33 -0700

    Update model renderer to include wireframe

--- js/modelviewer.js
+++ js/modelviewer.js
@@ -1,21 +1,21 @@
 THREE.Cache.enabled = true;
-var scenes = [];
-var cameras = [];
-var renderers = [];
-var spinning = 99;
+let scenes = [];
+let cameras = [];
+let renderers = [];
+let spinning = 99;
 
-function setAlive(scene) {
+setAlive = (scene) => {
 	spinning = scene;
 }
 
-function kill() {
+kill = () => {
 	spinning = 99;
 }
 
-function animate() {
-	i = 0;
+animate = () => {
+	let i = 0;
 	while (scenes.length > i) {
-		if (scenes[i].children.length > 1 && i == spinning) { 
+		if (scenes[i].children.length > 1 && i == spinning) {
     		scenes[i].children[1].rotation.y += 0.025;
 		}
 		renderers[i].render( scenes[i], cameras[i] );
@@ -24,12 +24,13 @@ function animate() {
 	requestAnimationFrame(animate);
 }
 
-var loader = new THREE.STLLoader();
-function renderModel(model, rotate) {
-	var scene = new THREE.Scene();
-	var camera = new THREE.PerspectiveCamera( 75, 320 / 300, 0.1, 1000 );
-	var renderer = new THREE.WebGLRenderer( {alpha: true } );
-	var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+let loader = new THREE.STLLoader();
+
+renderModel = (model, rotate) => {
+	const scene = new THREE.Scene();
+	const camera = new THREE.PerspectiveCamera( 75, 320 / 300, 0.1, 1000 );
+	const renderer = new THREE.WebGLRenderer( {alpha: true } );
+	const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
 
 	renderer.setSize( 250, 250 );
 	renderer.setClearColor( 0x000000, 0 );
@@ -46,15 +47,23 @@ function renderModel(model, rotate) {
 	model = "models/" + model + ".stl";
 
 	loader.load(model, function ( geometry ) {
-		if (scene.children.length > 1){ 
-			scene.remove(scene.children[1]); 
+		if (scene.children.length > 1){
+			scene.remove(scene.children[1]);
 		}
 		geometry.computeBoundingSphere();
 		camera.position.z = geometry.boundingSphere.radius + geometry.boundingSphere.radius / 5;
 		camera.position.y = geometry.boundingSphere.radius + geometry.boundingSphere.radius / 1.5;
 		light.position.y = geometry.boundingSphere + 10;
 		if (rotate) geometry.rotateX(-1.5708);
-		scene.add( new THREE.Mesh( geometry ) );
+
+		const mesh = new THREE.Mesh(geometry);
+
+		const geo = new THREE.EdgesGeometry( mesh.geometry );
+		const mat = new THREE.LineBasicMaterial( { color: 0x333333, linewidth: 2 } );
+		const wireframe = new THREE.LineSegments( geo, mat );
+		mesh.add(wireframe);
+
+		scene.add(mesh);
 	});
 }
 
