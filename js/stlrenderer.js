c805a823a8c98c9a215b80effdd8a652e8329faf
Jordan Whiting <jordandavidwhiting@gmail.com>
2019-06-23 07:35:37 -0700

    Update stlrenderer.js

--- js/stlrenderer.js
+++ js/stlrenderer.js
@@ -1,7 +1,7 @@
 THREE.Cache.enabled = true;
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight * 1.5, 0.1, 1000 );
+const camera = new THREE.OrthographicCamera( 75, window.innerWidth / window.innerHeight * 1.5, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer( {alpha: true } );
 const controls = new THREE.OrbitControls( camera, renderer.domElement );
 controls.enablePan = false;
